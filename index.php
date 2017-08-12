e8f5706de2d3dc2ddc119293da2a5706ba8b84d5
RafaRodrigu3s <raflerodrigues@gmail.com>
2017-08-12 14:52:02 -0300

    adicionando index

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>RafaRodrigues</title>
     <link rel="stylesheet" type="text/css" href="css/app.css">
 
-    
+
   </head>
 
   <body>
@@ -30,19 +30,134 @@
             <li><a href="#about">About</a></li>
             <li><a href="#contact">Contact</a></li>
           </ul>
+          <!--icon-->
+          <ul class="nav navbar-nav navbar-right">
+            <li ><a href="http://twitter.com"><i class="fa fa-twitter"></i></a></li>
+            <li ><a href="http://facebook.com"><i class="fa fa-facebook"></i></a></li>
+            <li ><a href="http://instagram.com"><i class="fa fa-instagram" ></i></a></li>
+          </ul>
+
         </div><!--/.nav-collapse -->
       </div>
     </nav>
 
-    <div class="container">
+   <div class="showcase">
+     <div class="container">
+       <h1>Ideia</h1>
+       <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam imperdiet nec nisl at tempor. Praesent euismod nisi ut purus interdum, quis aliquam risus blandit. Phasellus sed elit sed ante facilisis ultricies. Pellentesque vel neque eget magna rutrum faucibus. Ut vel auctor tellus. Nam vel dui et ipsum accumsan congue eu at tortor. Donec semper quam et mi semper, non mattis mi viverra. Nulla ultrices quam volutpat, facilisis dui at, fringilla neque. Vivamus hendrerit tristique magna vitae aliquet.</p>
+       <a href="about.html" class="btn btn-primary">Read More</a>
+     </div>
+   </div>
 
-      <div class="starter-template">
-        <h1>Bootstrap starter template</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+    <div class="section-a">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <i class="fa fa-html5"></i>
+            <h3>Clen & Simple Code</h3>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam imperdiet nec nisl at tempor. Praesent euismod nisi ut purus interdum, quis aliquam risus blandit. Phasellus sed elit sed ante facilisis ultricies. Pellentesque vel neque eget magna rutrum faucibus. Ut vel auctor tellus. Nam vel dui et ipsum accumsan congue eu at tortor. Donec semper quam et mi semper, non mattis mi viverra. Nulla ultrices quam volutpat, facilisis dui at, fringilla neque. Vivamus hendrerit tristique magna vitae aliquet.</p>
+            </div>
+            <div class="col-md-4">
+              <i class="fa fa-gear"></i>
+              <h3>Sass PreCompiler</h3>
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam imperdiet nec nisl at tempor. Praesent euismod nisi ut purus interdum, quis aliquam risus blandit. Phasellus sed elit sed ante facilisis ultricies. Pellentesque vel neque eget magna rutrum faucibus. Ut vel auctor tellus. Nam vel dui et ipsum accumsan congue eu at tortor. Donec semper quam et mi semper, non mattis mi viverra. Nulla ultrices quam volutpat, facilisis dui at, fringilla neque. Vivamus hendrerit tristique magna vitae aliquet.</p>
+            </div>
+            <div class="col-md-4">
+              <i class="fa fa-lightbulb-o"></i>
+              <h3>Customizeble</h3>
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam imperdiet nec nisl at tempor. Praesent euismod nisi ut purus interdum, quis aliquam risus blandit. Phasellus sed elit sed ante facilisis ultricies. Pellentesque vel neque eget magna rutrum faucibus. Ut vel auctor tellus. Nam vel dui et ipsum accumsan congue eu at tortor. Donec semper quam et mi semper, non mattis mi viverra. Nulla ultrices quam volutpat, facilisis dui at, fringilla neque. Vivamus hendrerit tristique magna vitae aliquet.</p>
+            </div>
+          </div>
+        </div>
       </div>
 
-    </div><!-- /.container -->
-    
+      <div class="section-b">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                <div class="panel panel-default">
+                  <div class="panel-heading" role="tab" id="headingOne">
+                    <h4 class="panel-title">
+                      <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        Collapsible Group Item #1
+                      </a>
+                    </h4>
+                  </div>
+                  <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                    <div class="panel-body">
+                      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    </div>
+                  </div>
+                </div>
+                <div class="panel panel-default">
+                  <div class="panel-heading" role="tab" id="headingTwo">
+                    <h4 class="panel-title">
+                      <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        Collapsible Group Item #2
+                      </a>
+                    </h4>
+                  </div>
+                  <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+                    <div class="panel-body">
+                      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    </div>
+                  </div>
+                </div>
+                <div class="panel panel-default">
+                  <div class="panel-heading" role="tab" id="headingThree">
+                    <h4 class="panel-title">
+                      <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        Collapsible Group Item #3
+                      </a>
+                    </h4>
+                  </div>
+                  <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                    <div class="panel-body">
+                      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              </div>
+              <div class="col-md-6">
+                  <img src="img/urso.jpg" alt="urso">
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="section-c">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-8 col-md-offfset-2">
+                <i class="fa fa-html5"></i>
+                <h2>SubScrib To Our Newsletter</h2>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam imperdiet nec nisl at tempor. Praesent euismod nisi ut purus interdum, quis aliquam risus blandit. Phasellus sed elit sed ante facilisis ultricies. Pellentesque vel neque eget magna rutrum faucibus. Ut vel auctor tellus. Nam vel dui et ipsum accumsan congue eu at tortor. Donec semper quam et mi semper, non mattis mi viverra. Nulla ultrices quam volutpat, facilisis dui at, fringilla neque. Vivamus hendrerit tristique magna vitae aliquet.</p>
+                <hr/>
+                <form >
+                  <input type="text" class="form-control input-lg"  placeholder="Enter Email">
+                  <br/>
+                  <button class="btn btn-primary btn-lg btn-block">Submit</button>
+                </form>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <footer>
+      <div class="container">
+          <p>2017 - Desenvolvido por Rafael Rodrigues - Todos os direitos reservados</p>
+      </div>
+    </footer>
+
+
+      <script src="bower_components/jquery/dist/jquery.js"></script>
+    <script src="bower_components/bootstrap-sass/assets/javascripts/bootstrap.js"></script>
+
+
 
   </body>
 </html>
